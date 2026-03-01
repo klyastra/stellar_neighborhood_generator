2e7f53d5bed5a72be4a3517f065b353cbe8de53a
kly <kly@kly2005.localdomain>
2026-03-01 14:12:59 -0800

    Derived stellar attributes (temp, radius, etc.) in .csv output file.

--- stellar_neighborhood_generator/stargen.py
+++ stellar_neighborhood_generator/stargen.py
@@ -95,19 +95,27 @@ plt.show()
 
 
 # Save samples as CSV
-combined_samples = np.column_stack((samples_imf, samples_age))
-np.savetxt('outputs/output.csv', combined_samples, delimiter='   ', header='Mass [Msol]   Age [Gyr]', comments='')
+### Use the class "Star" to create a list of stars
+MS_bool, samples_radii, samples_temp, samples_lum = [], [], [], []
+
+for i in range(n_samples):
+    star = Star(f"Star {i+1}", samples_imf[i], samples_age[i])
+
+    MS_bool.append(star.main_sequence)
+    samples_radii.append(star.radius)
+    samples_temp.append(star.temperature)
+    samples_lum.append(star.luminosity)
+
+'''
+for star in stars:
+    print(f"{star.name}, {star.initial_mass:.3f} Msol, {star.age:.3f} Gyr, Main Sequence? ({star.main_sequence}), {star.radius:.3f} Rsol, {star.temperature:.0f} K, {star.luminosity:.3f} Lsol")
+'''
+
+output = np.column_stack((samples_imf, samples_age,
+                        np.array(MS_bool), np.array(samples_radii), np.array(samples_temp), np.array(samples_lum)))
+
+np.savetxt('outputs/output.csv', output, delimiter='  ',
+    header='Mass_[Msol]  Age_[Gyr]  Main-Seq_flag  Radius_[Rsol]  Temperature_[K]  Luminosity_[Lsol]')
 
 
 print(f"Samples of {n_samples} stars have been saved to the 'outputs' folder.")
-
-
-
-
-####  Use the class "Star" to create a list of stars
-stars = []
-for i in range(10):
-    stars.append(Star(f"Star {i+1}", samples_imf[i], samples_age[i]))
-
-for star in stars:
-    print(f"{star.name}, {star.initial_mass:.3f} Msol, {star.age:.3f} Gyr, Main Sequence? ({star.main_sequence}), {star.radius:.3f} Rsol, {star.temperature:.0f} K, {star.luminosity:.3f} Lsol")
